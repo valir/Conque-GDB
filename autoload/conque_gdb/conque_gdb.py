4bbd5b2daa32b7e349b8bdf6dbadf3db469b3e01
Valentin Rusu <valir@rusu.info>
2022-05-22 11:54:29 +0300

    MutableMapping now is in collections.abc module

--- autoload/conque_gdb/conque_gdb.py
+++ autoload/conque_gdb/conque_gdb.py
@@ -36,7 +36,7 @@ class RegisteredBreakpoint:
     def __str__(self):
         return self.filename + ':' + self.lineno + ',' + self.enabled
 
-class RegisteredBpDict(collections.MutableMapping):
+class RegisteredBpDict(collections.abc.MutableMapping):
     def __init__(self):
         self.r_breaks = dict()
         self.lookups = dict()
@@ -102,7 +102,7 @@ class ConqueGdb(Conque):
     # Breakpoints which have been registered to exist
     registered_breakpoints = RegisteredBpDict()
 
-    # Mapping from linenumber + filename to a tuple containing the id of the sign 
+    # Mapping from linenumber + filename to a tuple containing the id of the sign
     # placed there and whether the breakpoint is enabled ('y') or disabled ('n')
     lookup_sign_ids = dict()
 
